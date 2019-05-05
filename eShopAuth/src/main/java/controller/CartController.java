cdd98f69c8e3e258dcfd655430ce13d54ef01c86
dimmxx <dimmxx@gmail.com>
2019-05-05 18:53:45 +0300

    05-05-2019 mac

--- eShopAuth/src/main/java/controller/CartController.java
+++ eShopAuth/src/main/java/controller/CartController.java
@@ -11,11 +11,11 @@ public class CartController {
         this.cart = cart;
     }
 
-    public void addProductToCartMap(Product product){
+    public void addProductToCartMap(Product product, int quantity){
         if(cart.getProductMap().containsKey(product)){
-            cart.getProductMap().put(product, cart.getProductMap().get(product) + 1);
+            cart.getProductMap().put(product, cart.getProductMap().get(product) + quantity);
             } else {
-            cart.getProductMap().put(product, 1);
+            cart.getProductMap().put(product, quantity);
         }
 
     }
--- eShopAuth/src/main/java/servlet/CartServletMulti.java
+++ eShopAuth/src/main/java/servlet/CartServletMulti.java
@@ -6,20 +6,25 @@ import controller.ProductController;
 import model.Cart;
 import model.Product;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.logging.Logger;
 
-public class CartServlet extends HttpServlet {
+public class CartServletMulti extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         Cart cart = null;
+        //int productId = Integer.valueOf(req.getParameter("productId"));
+        //int quantity = Integer.valueOf(req.getParameter("quantity"));
+
+        String productId = req.getParameter("productId");
+        String quantity = req.getParameter("quantity");
+
         HttpSession session = req.getSession();
         if (session.getAttribute("cart") == null){
             cart = new Cart();
@@ -29,26 +34,13 @@ public class CartServlet extends HttpServlet {
 
         CartController cartController = new CartController(cart);
 
-        String productId = req.getParameter("productId");
-
-        if (productId != null) {
+        if (productId != null && quantity != null) {
             DbWorker db = new DbWorker();
             ProductController productController = new ProductController(db);
             Product product = productController.getProduct(Integer.valueOf(productId));
-            cartController.addProductToCartMap(product, 1);
+            cartController.addProductToCartMap(product, Integer.valueOf(quantity));
             session.setAttribute("cart", cartController.getCart());
         }
-
-        resp.sendRedirect("./");
-
-    }
-
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        RequestDispatcher rd = req.getRequestDispatcher("WEB-INF/view/cartView.jsp");
-        rd.forward(req, resp);
 
     }
 }
